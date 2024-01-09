c263a6983d4b621b6dc5e3f1ccf2b71be6579960
Tomas Zeman <tomaas.zeman@gmail.com>
2024-01-09 22:44:56 +0100

    Formatting

--- level01/01-re-id/solution.py
+++ level01/01-re-id/solution.py
@@ -7,7 +7,7 @@ def generate_primes(n):
     numbers = [True] * n
     for i in range(2, int(sqrt(n))):
         if numbers[i]:
-            for j in range(i**2, n, i):
+            for j in range(i ** 2, n, i):
                 numbers[j] = False
     return [str(i) for i, value in enumerate(numbers) if value and i > 1]
 
@@ -15,7 +15,7 @@ def generate_primes(n):
 def solution(i):
     # 20233 is precomputed number that gives us the string length enough
     # to work with our 10000 minions
-    return "".join(generate_primes(20233))[i : i + ID_LENGTH]
+    return "".join(generate_primes(20233))[i: i + ID_LENGTH]
 
 
 assert solution(0) == "23571"
